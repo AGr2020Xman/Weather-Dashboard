9d76d85c02ea06877d9fe35b37df56f0c5b20be6
Andre Grech <silver.grech@gmail.com>
2020-09-21 17:53:26 +1000

    Both Ajax calls are functioning

--- script.js
+++ script.js
@@ -1,30 +1,5 @@
 // first API call to currentWeather responses
 
-// root ARRAY
-var weatherCall = response.weather;
-
-// (.png)
-var weatherIconVal = weatherCall[0].icon;
-
-// access coords
-var weatherCoord = response.coord;
-
-var longitude = weatherCoord.lon;
-
-var latitude = weatherCoord.lat;
-
-// open wind obj
-var weatherWind = response.wind;
-// windspeed in (mph? or kph?)
-var weatherWindSpeed = weatherWind.speed;
-
-// daily weather in SECOND api call -
-var weatherDaily = response.daily;
-
-// uv index value (green0-2, yellow3-5, orange6-7, red8-10, violet11+)
-var uvIndex = response.current.uvi;
-
-var cityInput = $("#searchInput").trim().val();
 // maximum 7 days
 var forecastDayTarget = 5;
 
@@ -37,7 +12,7 @@ const ApiKey = "3ff9623f9027960becbeadb447702b80";
 
 var currentWeatherURL = (cityInput) => {
   return (
-    "api.openweathermap.org/data/2.5/weather?q=" +
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
     cityInput +
     // &units=metric || &units=imperial
     "&appid=" +
@@ -57,49 +32,70 @@ var createURL2 = (latitude, longitude) => {
   );
 };
 
-$("#search").click();
-
-var oneAPICall = (latitude, longitude, weatherDaily) => {
+console.log("Debug");
+const oneAPICall = (latitude, longitude) => {
   $.ajax({
     url: createURL2(latitude, longitude),
     method: "GET",
-  }).then(dailyForecastRetrieval(weatherDaily));
+  }).then(function (response) {
+    // uv index value (green0-2, yellow3-5, orange6-7, red8-10, violet11+)
+    var uvIndex = response.current.uvi;
+    // daily weather in SECOND api call -
+    var weatherDaily = response.daily;
+    console.log("UV Index", uvIndex);
+    console.log(weatherDaily[0].dt);
+    console.log(weatherDaily[1].dt);
+    console.log(weatherDaily[2].dt);
+    console.log(weatherDaily[3].dt);
+    console.log(weatherDaily[4].dt);
+  });
 };
 
-var searchCallToAPI = () => {
+const searchCallToAPI = () => {
+  console.log("words?");
+  var cityInput = $("#searchInput").val();
   $.ajax({
     url: currentWeatherURL(cityInput),
     method: "GET",
   }).then((response) => {
+    console.log(response);
+
     var weatherStats = response.main;
     var cityName = response.name;
     // in Kelvin - (toggle F<>C)
     var weatherStats = response.main;
     var currentWeatherTemp = weatherStats.temp;
     var weatherHumidity = weatherStats.humidity;
+    // root ARRAY
+    var weatherCall = response.weather;
 
-    console.log(currentWeatherTemp);
-    console.log(weatherHumidity);
-    console.log(weatherWindSpeed);
-    console.log(longitude);
-    console.log(latitude);
-    console.log(cityName);
+    // (.png)
+    var weatherIconVal = weatherCall[0].icon;
 
-    oneAPICall(latitude, longitude, weatherDaily);
+    // access coords
+    var weatherCoord = response.coord;
+
+    var longitude = weatherCoord.lon;
+
+    var latitude = weatherCoord.lat;
+
+    // open wind obj
+    var weatherWind = response.wind;
+    // windspeed in (mph? or kph?)
+    var weatherWindSpeed = weatherWind.speed;
+
+    console.log("CurrentTemp", currentWeatherTemp);
+    console.log("Humidity", weatherHumidity);
+    console.log("Wind Speed", weatherWindSpeed);
+    console.log("Longitude", longitude);
+    console.log("Latitude", latitude);
+    console.log("City", cityName);
+
+    oneAPICall(latitude, longitude);
   });
 };
 
-function() {
-    dbkfjhhdeskjh
-    function() {
-        dhfgkjfdehgkjdfhgk
-        function() {
-
-        }
-    }
-}
-
-var dailyForecastRetrieval = () => {
+const dailyForecastRetrieval = () => {
   for (i = 0; i < forecastDayTarget; i++) {
     // daily main temp
     var dailyTemp = weatherDaily[i].temp.day;
@@ -115,7 +111,9 @@ var dailyForecastRetrieval = () => {
   }
 };
 
-var populateCityData = (city, savedCity) => {
+const populateCityData = (city, savedCity) => {
   searchCallToAPI(response);
   var nowMoment = moment();
 };
+
+$("#search").click(searchCallToAPI);
